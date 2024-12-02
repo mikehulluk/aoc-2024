b077ceed37660e3fca6f5d93c10153dd20b766d6
Mike Hull <m.hull@flockmobility.com>
2024-12-02 18:41:10 +0000

    Cleanup

--- day01/app/Main.hs
+++ day01/app/Main.hs
@@ -1,25 +1,23 @@
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Prelude
 
-
 import Debug.Trace
 import Data.List (transpose, sort)
 import Control.Monad
-import Flow ((|>))
+import Data.Map as Map
+
 
 type LocationId = Int
 type LocationIdList = [LocationId]
 
 
-
 main :: IO ()
 main = do
-    --locations <- loadLocationFile "test-input-0.txt" 
+    -- locations <- loadLocationFile "test-input-0.txt" 
     locations <- loadLocationFile "test-input-1.txt" 
 
     when (length locations /= 2) $ error "Bad input lengths (I)"
@@ -32,8 +30,11 @@ main = do
     traceM $ "List0: " ++ show lst0
     traceM $ "List1: " ++ show lst1
 
-    let dist = listsDistance lst0 lst1
-    putStrLn $ "List distance: " ++ show dist
+    let dist1 = listsDistancePt1 lst0 lst1
+    putStrLn $ "List distance1: " ++ show dist1
+
+    let dist2 = listsDistancePt2 lst0 lst1
+    putStrLn $ "List distance1: " ++ show dist2
 
     pure ()
 
@@ -53,9 +54,36 @@ loadLocationFile pth = do
         pure inputDataByCol
         
 
-listsDistance :: LocationIdList -> LocationIdList -> Int
-listsDistance lst0 lst1 = 
+listsDistancePt1 :: LocationIdList -> LocationIdList -> Int
+listsDistancePt1 lst0 lst1 = 
         zip (sort lst0) (sort lst1)
             |> fmap (\(l0, l1) -> abs  (l0 - l1)) 
             |> sum
 
+
+listsDistancePt2 :: LocationIdList -> LocationIdList -> Int
+listsDistancePt2 lst0 lst1 = 
+        lst0
+            |> fmap (\v -> (Map.findWithDefault 0 v lst1Counts) *v)
+            |> sum
+
+    where lst1Counts = countMap lst1
+
+
+
+
+
+
+
+
+
+countMap :: (Ord a) => [a] -> Map a Int
+countMap elems =
+    elems 
+        |> fmap (\k -> Map.singleton k 1)
+        |> Map.unionsWith (+)
+
+
+infixl 0 |>
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
